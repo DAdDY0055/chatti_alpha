3c8018de6ca5cd4567bac3b16a9dbbe67f4b7f94
Takuya <yky2800.rthm.55tn@gmail.com>
2019-09-07 15:51:33 +0900

    portを変える

--- chatti_alpha/config/dev.exs
+++ chatti_alpha/config/dev.exs
@@ -16,7 +16,7 @@ config :chatti_alpha, ChattiAlpha.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :chatti_alpha, ChattiAlphaWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4200],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
